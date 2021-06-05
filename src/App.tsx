deed8b3ca18c705d4b8d767f10d22b6230c539bc
BadaHertz52 <lmlwpeace@naver.com>
2021-06-05 23:18:15 +0900

    create Todo List
    - todo list 에 대한 리더스 모듈, 스토어, 컨테이너 컴포넌트, 프레젠테이셔널 컴포넌트를 생성
    - app 에 TodoApp 추가 및 css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import CounterContainer from './container/CounterContainer';
+import TodoApp from './container/TodoApp';
 
 
 function App () {
-  return <CounterContainer/>
+  const Appstyle :React.CSSProperties  = {
+    marginLeft: 200,
+    marginTop: 100
+  };
+  return (
+    <>
+      <div style = {Appstyle}>
+        <h1>COUNTER</h1>
+        <CounterContainer/>
+      </div>
+      
+      <div style = {Appstyle} >
+        <h1>Todo Lsit</h1>
+        <TodoApp/> 
+      </div>
+      
+      
+    </>
+  )
+  
 }
 
 export default App;
